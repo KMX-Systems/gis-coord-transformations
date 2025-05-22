f2df1384d88696340aa73cf65dab25a2789f38c1
Flaviu <cflaviu@users.noreply.github.com>
2025-05-22 09:14:22 +0300

    improvement

--- source/library/inc/kmx/gis.hpp
+++ source/library/inc/kmx/gis.hpp
@@ -1,10 +1,11 @@
 /// Copyright (c) 2025 - present KMX Systems. All rights reserved.
 /// @file kmx/gis.hpp
-/// @brief Defines core constants, data structures, and conversion functions for GIS operations.
+/// @brief Header that includes all GIS functionality defined by this library.
 ///
 /// @note This file assumes a C++20 compliant compiler.
 /// @warning Does not use PCH if PCH is not defined.
 #pragma once
 #ifndef PCH
     #include <kmx/gis/coordinate/conversion.hpp>
+    #include <kmx/gis/uniform_grid_mapper.hpp>
 #endif
